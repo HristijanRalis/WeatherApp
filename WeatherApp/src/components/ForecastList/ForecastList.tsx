8433b2281646f792ae3a1e6fad5e0f64cbf615bd
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-12-17 10:57:46 +0100

    Added starter folders with components, services and types  & created the interfaces

---
+++ WeatherApp/src/components/ForecastList/ForecastList.tsx
@@ -0,0 +1,3 @@
+export const ForecastList = () => {
+  return <div>ForecastList</div>;
+};
---
+++ WeatherApp/src/types/weather.ts
@@ -0,0 +1,22 @@
+export interface WeatherItem {
+  dt: number;
+  dt_txt: StorageManager;
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+  };
+  weather: {
+    main: string;
+    icon: string;
+    description: string;
+  };
+}
+
+export interface WeatherResponse {
+  list: WeatherItem[];
+  city: {
+    name: string;
+    country: string;
+  };
+}
